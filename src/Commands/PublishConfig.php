b5a11cbd5785cb34ec99028550d2544ab6f5c4b8
Linus Metzler <hi@linusmetzler.me>
2022-12-10 20:27:39 +0000

    support local config

--- src/Commands/PublishConfig.php
+++ src/Commands/PublishConfig.php
@@ -12,11 +12,33 @@ class PublishConfig extends Command
 
     public function handle(): int
     {
+        if (! file_exists($this->filename('pint.dist.json.orig'))) {
+            copy($this->filename('pint.dist.json'), $this->filename('pint.dist.json.orig'));
+        }
+
+        $dist = json_decode(file_get_contents($this->filename('pint.dist.json.orig')), true);
+        $local = file_exists(base_path('pint.local.json'))
+            ? json_decode(file_get_contents(base_path('pint.local.json')), true)
+            : [];
+
+        $merged = $dist;
+        $merged['rules'] = [
+            ...$merged['rules'],
+            ...($local['rules'] ?? []),
+        ];
+
+        file_put_contents($this->filename('pint.dist.json'), json_encode($merged, JSON_PRETTY_PRINT));
+
         $this->call('vendor:publish', [
             '--tag' => 'laravel-pint-config',
             '--force' => true,
         ]);
 
         return self::SUCCESS;
+    }
+
+    private function filename(string $name): string
+    {
+        return sprintf(__DIR__.'/../../%s', $name);
     }
 }
